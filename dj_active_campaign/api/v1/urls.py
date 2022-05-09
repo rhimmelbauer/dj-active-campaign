72b252cc8b9b0859b127fb2f66f558489aa6fd98
Roberto Himmelbauer <robertoh89@gmail.com>
2022-05-09 10:16:03 -0700

    added contact update endpoint

--- dj_active_campaign/api/v1/urls.py
+++ dj_active_campaign/api/v1/urls.py
@@ -7,4 +7,5 @@ app_name = "dj_active_campaign_api"
 urlpatterns = [
     path('custom/field/<str:uuid>/create/', views.ActiveCampaignCustomFieldCreate.as_view(), name='ac-custom-field-create'),
     path('contact/create/', views.ActiveCampaignContactCreate.as_view(), name='ac-contact-create'),
+    path('contact/update/', views.ActiveCampaignContactUpdate.as_view(), name='ac-contact-update'),
 ]
--- dj_active_campaign/api/v1/views.py
+++ dj_active_campaign/api/v1/views.py
@@ -1,6 +1,7 @@
 import json
 import logging
 
+from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from django.views.generic import View
 from django.urls import reverse_lazy
@@ -45,8 +46,18 @@ class ActiveCampaignContactCreate(View):
         if not contact_api.is_response_valid():
             logger.error(contact_api.errors)
 
-            return redirect(request.META.get('HTTP_REFERER', self.default_redirect))
-
         return redirect(request.META.get('HTTP_REFERER', self.default_redirect))
 
 
+class ActiveCampaignContactUpdate(View):
+    default_redirect = reverse_lazy('dj-active-campaign-index')
+
+    def post(self, request, *args, **kwargs):
+        contact_api = ContactAPI(get_site_from_request(request))
+
+        contact_api.update(json.loads(request.body))
+
+        if not contact_api.is_response_valid():
+            logger.error(contact_api.errors)
+        
+        return redirect(request.META.get('HTTP_REFERER', self.default_redirect))
