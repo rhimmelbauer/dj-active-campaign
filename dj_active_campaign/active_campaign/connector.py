81be729f02f426493965f82fb8528c9e9729517c
Roberto Himmelbauer <robertoh89@gmail.com>
2022-05-11 15:12:38 -0700

    added instance when initializing the connector duh

--- dj_active_campaign/active_campaign/connector.py
+++ dj_active_campaign/active_campaign/connector.py
@@ -16,9 +16,9 @@ class ActiveCampaignConnector(object):
         active_campaign_credentials = ActiveCampaignIntegration(site)
 
         if active_campaign_credentials.instance:
-            self.url = active_campaign_credentials.client_url + active_campaign_credentials.client_id
+            self.url = active_campaign_credentials.instance.client_url + active_campaign_credentials.instance.client_id
             self.headers = {
-                'Api-Token': active_campaign_credentials.private_key,
+                'Api-Token': active_campaign_credentials.instance.private_key,
                 'Content-Type': 'application/json'
             }
         elif settings.ACTIVE_CAMPAIGN_URL and settings.ACTIVE_CAMPAIGN_KEY and settings.ACTIVE_CAMPAIGN_VERSION:
